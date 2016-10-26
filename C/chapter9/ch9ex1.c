1250b4bafe18ae1de8988972f9af9bb2cdfba925
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2016-10-25 20:02:34 -0400

    ch9ex1 done.

--- C/chapter9/ch9ex1.c
+++ C/chapter9/ch9ex1.c
@@ -3,17 +3,18 @@
 struct money{
 	int dollars;
 	int cents;
-}
+};
 
 struct money init(int dollars, int cents);
-
 struct money add(struct money a, struct money b);
+
+struct money add(struct money a, struct money b)
 { 
 	int dollars = a.dollars + b.dollars;
 	int cents = a.cents + b.cents;
 	if(cents > 99){
 		int remaining_cents = cents - 100;
-		int dollars_in_change = 100 - remaining_cents;
+		int dollars_in_change = cents % 100;
 		cents = remaining_cents;
 		dollars += dollars_in_change;
 	}
@@ -31,5 +32,10 @@ struct money init(int dollars, int cents)
 
 int main(void)
 {
-	
+	struct money mymoney = init(20, 50);
+	struct money yourmoney = init(30, 51);
+	printf("%d.%d\n", mymoney.dollars, mymoney.cents);
+	printf("%d.%d\n", yourmoney.dollars, yourmoney.cents);
+	struct money totalmoney = add(mymoney, yourmoney);
+	printf("%d.%d\n", totalmoney.dollars, totalmoney.cents);
 }
